5174d5e9e2eb666372be1e91b2a228b3d565032f
SebastianMestre <mestresebastianunr@gmail.com>
2022-09-24 21:01:20 -0300

    refactor magistral

--- estructuras/segtree_lazy.cpp
+++ estructuras/segtree_lazy.cpp
@@ -1,43 +1,38 @@
-
 struct Lazy {
 	// TODO agregar data
+	static Lazy zero() { /* TODO */ } // neutro de la suma
+	bool is_zero() { /* TODO */ } // moralmente `*this == Lazy::zero()`
 };
 
 struct Mono {
 	// TODO agregar data
-	Lazy lazy;
-	static Mono zero() { return ; /* TODO */ }
-	bool has_lazy() { return ; /* TODO */ }
-	void add_lazy(Lazy l) { /* TODO */ }
-	void clear_lazy() { /* TODO */ }
-	void apply_lazy(int l, int r) { /* TODO */ }
+	static Mono zero() { /* TODO */ } // neutro de la suma
+	void apply_lazy(Lazy d, int l, int r) { /* TODO */ }
 };
 
-Mono operator+ (Mono a, Mono b) {
-	assert(!a.has_lazy());
-	assert(!b.has_lazy());
-	return ; // TODO
-}
+Lazy operator+ (Lazy a, Lazy b) { /* TODO */ } // asociativo
+Mono operator+ (Mono a, Mono b) { /* TODO */ } // asociativo
+
 
 struct SegtreeLazy {
 	static constexpr int log2n = 17; // TODO
 	static constexpr int ln = 1<<log2n;
 	static constexpr int sz = ln<<1;
 
-	vector<Mono> data;
-
-	SegtreeLazy() : data(sz) {
-	}
+	vector<Mono> data; vector<Lazy> lazy;
+	SegtreeLazy() : data(sz), lazy(sz) { }
 
 	// O(N)
 	// inicia la estructura
 	void init_all() {
 		forn(i, sz) data[i] = Mono::zero();
+		forn(i, sz) lazy[i] = Lazy::zero();
 	}
 
 	// O(1)
 	// asigna un valor sin actualizar la estructura
 	// se usa con init_inner para hacer O(N) point updates en O(N)
+	// NO TOMA EN CUENTA LAZY UPDATES PENDIENTES
 	void assign_unsafe(int i, Mono x) {
 		i += ln; data[i] = x;
 	}
@@ -69,13 +64,13 @@ struct SegtreeLazy {
 	int ql, qr;
 
 	void p_(int i, int l, int r) {
-		if (!data[i].has_lazy()) return;
-		data[i].apply_lazy(l, r);
+		if (lazy[i].is_zero()) return;
 		if (i < ln) {
-			data[2*i].add_lazy(data[i].lazy);
-			data[2*i+1].add_lazy(data[i].lazy);
+			lazy[2*i] = lazy[2*i] + lazy[i];
+			lazy[2*i+1] = lazy[2*i+1] + lazy[i];
 		}
-		data[i].clear_lazy();
+		data[i].apply_lazy(lazy[i], l, r);
+		lazy[i] = Lazy::zero();
 	}
 
 	Mono q_(int i, int l, int r) {
@@ -92,7 +87,7 @@ struct SegtreeLazy {
 		p_(i,l,r);
 		if (r <= ql || qr <= l) return;
 		if (ql <= l && r <= qr) {
-			data[i].add_lazy(x);
+			lazy[i] = lazy[i] + x;
 			p_(i,l,r);
 			return;
 		}
@@ -101,3 +96,4 @@ struct SegtreeLazy {
 		data[i] = data[2*i] + data[2*i+1];
 	}
 };
+
