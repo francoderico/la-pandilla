a514cf8ebe6a08e03dad4b6b6d0e5f1169202aed
SebastianMestre <mestresebastianunr@gmail.com>
2022-09-26 10:13:50 -0300

    lo hace mas chiquito

--- estructuras/segtree_lazy_test.cpp
+++ estructuras/segtree_lazy_test.cpp
@@ -53,73 +53,38 @@ Mono operator+ (Mono a, Mono b) {
 
 
 struct SegtreeLazy {
-	static constexpr int log2n = 17;
-	static constexpr int ln = 1<<log2n;
-	static constexpr int sz = ln<<1;
-
+	static constexpr int log2n = 17; // TODO
+	static constexpr int len = 1<<log2n, sze = 1<<(log2n+1);
 	vector<Mono> data; vector<Lazy> lazy;
-	SegtreeLazy() : data(sz), lazy(sz) { }
-
-	// O(N)
-	// inicia la estructura
-	void init_all() {
-		forn(i, sz) data[i] = Mono::zero();
-		forn(i, sz) lazy[i] = Lazy::zero();
+	SegtreeLazy() : data(sze), lazy(sze) { }
+	void init(Mono* a, int n) { // inicia con los valores dados O(n+len)
+		forn(i, sze) data[i] = Mono::zero();
+		forn(i, sze) lazy[i] = Lazy::zero();
+		forn(i, n) data[i+len] = a[i];
+		dforr(i, 1, len) data[i] = data[i*2] + data[i*2+1];
 	}
-
-	// O(1)
-	// asigna un valor sin actualizar la estructura
-	// se usa con init_inner para hacer O(N) point updates en O(N)
-	// NO TOMA EN CUENTA LAZY UPDATES PENDIENTES
-	void assign_unsafe(int i, Mono x) {
-		i += ln; data[i] = x;
-	}
-
-	// O(N)
-	// recalcula todos los nodos internos de la estructura
-	// NO TOMA EN CUENTA LAZY UPDATES PENDIENTES
-	void init_inner() {
-		dforr(i, 1, ln)
-			data[i] = data[i*2] + data[i*2+1];
-	}
-
-	// O(logN)
-	// update en rango
-	void update(int l, int r, Lazy x) {
-		ql = l; qr = r;
-		u_(1, 0, ln, x);
-	}
-
-	// O(logN)
-	// query en rango
-	Mono query(int l, int r) {
-		ql = l; qr = r;
-		return q_(1, 0, ln);
-	}
-
-	private:
-
+	// range update O(log(len))
+	void update(int l, int r, Lazy x) { ql = l; qr = r; u_(1, 0, len, x); }
+	// range query O(log(len))
+	Mono query(int l, int r) { ql = l; qr = r; return q_(1, 0, len); }
+private:
 	int ql, qr;
-
 	void p_(int i, int l, int r) {
 		if (lazy[i].is_zero()) return;
-		if (i < ln) {
+		if (i < len) {
 			lazy[2*i] = lazy[2*i] + lazy[i];
 			lazy[2*i+1] = lazy[2*i+1] + lazy[i];
 		}
 		data[i].apply_lazy(lazy[i], l, r);
 		lazy[i] = Lazy::zero();
 	}
-
 	Mono q_(int i, int l, int r) {
-		if (l == r) return Mono::zero();
-		p_(i,l,r);
 		if (r <= ql || qr <= l) return Mono::zero();
+		p_(i,l,r);
 		if (ql <= l && r <= qr) return data[i];
 		int m = (l+r)/2;
 		return q_(i*2,l,m) + q_(i*2+1,m,r);
 	}
-
 	void u_(int i, int l, int r, Lazy x) {
 		if (l == r) return;
 		p_(i,l,r);
@@ -154,9 +119,8 @@ ll cant_ceros(int l, int r) {
 
 ll area_union(vector<rect> const& rects) {
 
-	st.init_all();
-	forn(i, st.ln) st.assign_unsafe(i, {0, 1});
-	st.init_inner();
+	vector<Mono> init(100000, {0,1});
+	st.init(init.data(), init.size());
 
 	vector<sweep> lines;
 
