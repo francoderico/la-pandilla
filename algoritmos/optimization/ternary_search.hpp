32dfc5a0f9e6f3a714205eeb0bea6e2d77447c18
Federico Bersano <bersanofede@gmail.com>
2023-02-25 20:48:06 -0300

    Agregar ternary search en enteros y reales
    Si bien el algoritmo es muy sencillo, me di cuenta que no lo tenemos en
    ningún lado, y si bien capaz se nos puede llegar a ocurrir la
    implementación en la prueba, es mejor no renegar tanto y ya tenerlo
    escrito, sobre todo en el caso de los enteros donde hay que tener
    cuidado cuando llegas a `l + 2 == r`. El de los reales es más
    intuitivo y no hay que renegar tanto, pero lo puse para tener ambos, y
    además ya que hay una pequeña diferencia entre ambos (un `+1` en
    alguna parte).

---
+++ algoritmos/optimization/ternary_search.hpp
@@ -0,0 +1,20 @@
+/// Return integer `a` such that `f(a) <= f(b)` for each integer `l <= b < r`.
+template<typename Int, class Function>
+Int ternary_search_integer(Int l, Int r, Function f) {
+	while (r - l >= 3) {
+		const Int third = (r - l) / 3, m1 = l + third, m2 = r - third;
+		if (f(m1) > f(m2)) {l = m1 + 1;} else {r = m2;}
+	}
+	if (r - l == 2 && f(l) > f(l + 1)) {++l;}
+	return l;
+}
+
+/// Return real `a` such that `f(a) <= f(b)` for each real `l <= b < r`.
+template<typename Real, class Function>
+Real ternary_search_real(Real l, Real r, Function f, int iters=256){
+	while (iters--) {
+		const Real third = (r - l) /3.0, m1 = l + third, m2 = r - third;
+		if (f(m1) > f(m2)) {l = m1;} else {r = m2;}
+	}
+	return l;
+}
