fba6577c1a107e9c3de76d1a30851232f9caa73d
francoderico <francoderico2001@gmail.com>
2023-03-06 20:49:13 -0300

    Agrega las 4 formas usuales de factorizar: sqrt, 2 con criba, y rho

---
+++ algoritmos/math/fact.cpp
@@ -0,0 +1,59 @@
+// O(sqrt n)
+void fact(ll n, map<ll, int> &F)
+{
+    for(ll p = 2; p*p <= n; p ++) while(n%p == 0) F[p] ++, n /= p;
+    if(n > 1) F[n] ++;
+}
+
+
+
+//------------------------------------------------------
+
+
+
+// 0 si es primo, algún primo que lo divide si no.
+const int MAXP = 1e7+100;
+int criba[MAXP];
+
+void crear_criba()
+{
+    for(int i = 2; i*i < MAXP; i ++) if(! criba[i]) for(int j = i*i; j < MAXP; j += i) criba[j] = i;
+}
+
+// n <= sz(criba) (1e7)
+// O(log n)
+void fact(ll n, map<ll, int> &F)
+{
+    while(criba[n]) F[criba[n]] ++, n /= criba[n];
+    if(n > 1) F[n] ++;
+}
+
+
+
+//------------------------------------------------------
+
+
+
+// Requiere criba
+vector<int> primos;
+void buscar_primos()    //¡¡crear_criba() primero!!
+{
+    forr(i, 2, MAXP) if(! criba[i]) primos.pb(i);
+}
+
+// n <= MAXP^2
+// O(pi(n)) (cantidad de primos)
+void fact(ll n, map<ll, int> &F)
+{
+    for(auto &p : primos) while(n%p == 0) F[p] ++, n /= p;
+    if(n > 1) F[n] ++;
+}
+
+
+
+//------------------------------------------------------
+
+
+
+// Ver Pollard's Rho
+// O((log n)^3)
