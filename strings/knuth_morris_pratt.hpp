e6d8b64c2bb7ca1f55734fe295a7905a145e19be
Federico Bersano <bersanofede@gmail.com>
2023-03-08 18:04:53 -0300

    Agregar Knuth-Morris-Pratt (#31)
    Decidí representarlo como un objeto en vez de como una función porque así es más fácil reutilizarlo si se quiere reutilizar el patrón con distintos textos.

---
+++ strings/knuth_morris_pratt.hpp
@@ -0,0 +1,27 @@
+/// Encapsulation of the Knuth-Morris-Pratt string matching algorithm.
+///
+/// It takes a single string, the pattern that will be used for running KMP.
+/// Because the pattern's preprocessing is only done once, it allows us to
+/// efficiently run KMP with multiple texts.
+template<typename Char=char>
+struct Kmp {
+	using str = basic_string<Char>;
+	const vector<int> pi;
+	const str pattern;
+	Kmp(const str& _pattern):
+		pi(move(prefix_function(_pattern))), pattern(_pattern){}
+	/// Return a vector with the start of each occurrence of `pattern` in `text`.
+	vector<int> matches(const str& text) const {
+		if (sz(pattern) > sz(text)) {return {};}
+		vector<int> occs;
+		const int m = sz(pattern), n = sz(text);
+		if (m == 0) {occs.push_back(0);}
+		int j = 0;
+		forn(i, n) {
+			while (j != 0 && text[i] != pattern[j]) {j = pi[j-1];}
+			if (text[i] == pattern[j]) {++j;}
+			if (j == m) {occs.push_back(i - j + 1);}
+		}
+		return occs;
+	}
+};
