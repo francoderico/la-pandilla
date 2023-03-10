2878612d104279604e4a8b4cf3115ce1b1f2992e
francoderico <francoderico2001@gmail.com>
2023-03-10 18:29:25 -0300

    Cambia espacios por tabs

--- algoritmos/math/fact.cpp
+++ algoritmos/math/fact.cpp
@@ -1,8 +1,9 @@
 // O(sqrt n)
 void fact(ll n, map<ll, int> &F)
 {
-    for(ll p = 2; p*p <= n; p ++) while(n%p == 0) F[p] ++, n /= p;
-    if(n > 1) F[n] ++;
+	F.clear();
+	for(ll p = 2; p*p <= n; p ++) while(n%p == 0) F[p] ++, n /= p;
+	if(n > 1) F[n] ++;
 }
 
 
@@ -17,15 +18,16 @@ int criba[MAXP];
 // 0 si es primo (o si es 1), algún primo que lo divide si no.
 void crear_criba()
 {
-    for(int i = 2; i*i < MAXP; i ++) if(! criba[i]) for(int j = i*i; j < MAXP; j += i) criba[j] = i;
+	for(int i = 2; i*i < MAXP; i ++) if(! criba[i]) for(int j = i*i; j < MAXP; j += i) criba[j] = i;
 }
 
 // n <= sz(criba) (1e7)
 // O(log n)
 void fact(ll n, map<ll, int> &F)
 {
-    while(criba[n]) F[criba[n]] ++, n /= criba[n];
-    if(n > 1) F[n] ++;
+	F.clear();
+	while(criba[n]) F[criba[n]] ++, n /= criba[n];
+	if(n > 1) F[n] ++;
 }
 
 
@@ -36,17 +38,18 @@ void fact(ll n, map<ll, int> &F)
 
 // Requiere criba
 vector<int> primos;
-void buscar_primos()    //¡¡crear_criba() primero!!
+void buscar_primos()	//¡¡crear_criba() primero!!
 {
-    forr(i, 2, MAXP) if(! criba[i]) primos.pb(i);
+	forr(i, 2, MAXP) if(! criba[i]) primos.pb(i);
 }
 
 // n <= MAXP^2
-// O(pi(n)) (cantidad de primos)
+// O(sz(p)) (cantidad de primos)
 void fact(ll n, map<ll, int> &F)
 {
-    for(auto &p : primos) while(n%p == 0) F[p] ++, n /= p;
-    if(n > 1) F[n] ++;
+	F.clear();
+	for(auto &p : primos) while(n%p == 0) F[p] ++, n /= p;
+	if(n > 1) F[n] ++;
 }
 
 
