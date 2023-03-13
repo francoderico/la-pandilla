359b0bd77c2a9fcbe29de835767f6ca057f22b74
Federico Bersano <bersanofede@gmail.com>
2023-03-12 23:06:39 -0300

    Agregar tests
    Agregué 3 tests ya que hay que chequear bastantes cosas acá.

---
+++ strings/suffix_automaton_test.cpp
@@ -0,0 +1,43 @@
+// https://cses.fi/problemset/task/2106
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(c) begin(c),end(c)
+#define forr(i,l,r) for(int i=int(l);i<int(r);++i)
+#define fore(e,c) for(const auto&e:(c))
+#define sz(c) (int((c).size()))
+#define fst first
+#define snd second
+
+using ll = long long;
+
+#include "suffix_automaton.hpp"
+
+void solve(const SuffixAutomaton<char>& sa) {
+	int start = 0, bestlen = -1;
+	forr(curr, 1, sa.size) {
+		const int len = sa.len[curr];
+		if (len > bestlen) {
+			if (sa.terminal_paths_from[curr] > 1) {
+				start = sa.firstpos[curr] + 1 - (bestlen = len);
+			}
+		}
+	}
+	if (bestlen == -1) {cout<<"-1\n";}
+	else{cout<<sa.text.substr(start, bestlen)<<'\n';}
+}
+
+void solve() {
+	string s;
+	cin>>s;
+	SuffixAutomaton<char> sa(s);
+	solve(sa);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	solve();
+}
