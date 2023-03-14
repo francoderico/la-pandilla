b935d9cca70b6838bfae7efabe4c97dfc06b464b
Federico Bersano <bersanofede@gmail.com>
2023-03-14 12:12:28 -0300

    Agregar Aho-Corasick automata (#39)
    La implementación quedó sorpresivamente corta (teniendo en cuenta toda la información que puede encapsular este automata). Como guarda mucha data para cada estado, acá los comentarios de cada campo son muy importantes para entender que significan (y si es necesario usarlo/modificarlo).

---
+++ strings/aho_corasick_test.cpp
@@ -0,0 +1,47 @@
+// https://codeforces.com/contest/963/problem/D
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fore(e,c) for(const auto&e:(c))
+#define forr(i,l,r) for(int i=int(l);i<int(r);++i)
+#define forn(i,n) forr(i,0,n)
+#define sz(c) (int((c).size()))
+#define fst first
+#define snd second
+
+#include "aho_corasick.hpp"
+
+void solve() {
+	string s;
+	cin>>s;
+	int q;
+	cin>>q;
+	AhoCorasick t;
+	vector<pair<int,string>> queries(q);
+	forn(i, q) {
+		cin>>queries[i].fst>>queries[i].snd;
+		t.insert(queries[i].snd);
+	}
+	t.complete_automaton();
+	vector<vector<int>> matches = t.match(s);
+	forn(i, q) {
+		const auto& [k, p] = queries[i];
+		const auto occs = matches[i];
+		if (sz(occs) < k) {cout<<"-1\n";}
+		else {
+			int l = 0, r = k - 1;
+			int ans = occs[r] - occs[l];
+			while ((++l, ++r) < sz(occs)) {ans = min(ans, occs[r] - occs[l]);}
+			ans += sz(p);
+			cout<<ans<<'\n';
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	solve();
+}
