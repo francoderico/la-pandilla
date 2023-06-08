93b43c30e80a07863b4f267a289e19e13258debf
francoderico <francoderico2001@gmail.com>
2023-06-08 20:37:22 -0300

    Encapsula Hopcroft en una estructura y quita líneas de más

--- algoritmos/flujo/hopcroft.cpp
+++ algoritmos/flujo/hopcroft.cpp
@@ -1,64 +1,36 @@
-//Matching maximo en bipartito en O(sqrt(V) E)
-//Caso especial de Dinic pero mas rapido, menos memoria y mas corto
-//Los vertices izquierdos estan numerados en [0, n), los derechos en [0, m)
-int n, m;               //SETEAR
-vector<int> g[MAXN];    // [0,n)->[0,m)
-int mt[MAXN], mt2[MAXN], ds[MAXN];  //mt2[0..n) contiene el matching n-m
-                                    //mt[0..m)  contiene el matching m-n
-
-bool bfs()
-{
-	queue<int> q;
-	memset(ds, -1, sizeof(ds));
-	forn(i, n) if(mt2[i]<0) ds[i] = 0, q.push(i);
-	bool r = false;
-	while(not q.empty())
-	{
-		int x = q.front(); q.pop();
-		for(int y:g[x])
-		{
-			if(mt[y]>=0 and ds[mt[y]]<0) ds[mt[y]] = ds[x]+1, q.push(mt[y]);
-			else if(mt[y]<0) r = true;
+// Matching maximo en bipartito en O(sqrt(V) E)
+// Los vertices izquierdos estan numerados en [0, n), los derechos en [0, m)
+struct Hopcroft{
+	int n, m;
+	vector<vector<int>> g;    // [0,n)->[0,m)
+	vector<int> mt, inv, ds;  // mt  : [0..n) -> [0..m) contiene el matching n-m
+	                          // inv : [0..m) -> [0..n) contiene el matching inverso	
+	Hopcroft(int n_, int m_) : n(n_), m(m_), g(n_) {}
+	bool bfs(){
+		ds.assign(n, -1); queue<int> q;
+		forn(i, n) if(mt[i]<0) ds[i] = 0, q.push(i);
+		bool r = false;
+		while(not q.empty()){
+			int x = q.front(); q.pop();
+			for(auto y:g[x]){
+				if(inv[y]>=0 and ds[inv[y]]<0) ds[inv[y]] = ds[x]+1, q.push(inv[y]);
+				else if(inv[y]<0) r = true;
+			}
 		}
+		return r;
 	}
-	return r;
-}
-
-bool dfs(int x)
-{
-	for(int y:g[x]) if(mt[y]<0 or (ds[mt[y]] == ds[x]+1 and dfs(mt[y])))
-	{
-		mt[y] = x; mt2[x] = y;
-		return true;
-	}
-	ds[x] = 1<<30;
-	return false;
-}
-
-int mm()
-{
-	int r = 0;
-	memset(mt, -1, sizeof(mt)); memset(mt2, -1, sizeof(mt2));
-	while(bfs())
-	{
-		forn(i, n) if(mt2[i]<0) r += dfs(i);
-	}
-	return r;
-}
-
-//TIRAR mm() ANTES
-//MM = max_flow = min_cut
-vector<pii> mm_edges()
-{
-	vector<pii> ret;
-
-	forn(x, n)	//x: todos los nodos izquierdos
-	{
-		if(mt2[x] != -1)
-		{
-			ret.pb({x, mt2[x]+n});
+	bool dfs(int x){
+		for(auto y:g[x]) if(inv[y]<0 or (ds[inv[y]] == ds[x]+1 and dfs(inv[y]))){
+			inv[y] = x; mt[x] = y;
+			return true;
 		}
+		ds[x] = 1<<30;
+		return false;
 	}
-
-	return ret;
-}
+	int mm(){
+		int r = 0;
+		inv.assign(m, -1), mt.assign(n, -1);
+		while(bfs()) forn(i, n) if(mt[i]<0) r += dfs(i);
+		return r;
+	}
+};
