25aac1ac502fd8c653f3575e5e10bab488654786
SebastianMestre <mestresebastianunr@gmail.com>
2023-07-03 21:33:47 -0300

    Hace indexed set generico

--- estructuras/indexed_set.cpp
+++ estructuras/indexed_set.cpp
@@ -1,14 +1,14 @@
 #include <ext/pb_ds/assoc_container.hpp> 
 using namespace __gnu_pbds;
-typedef ll tipo;
-typedef tree<tipo,null_type,less<tipo>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+template<typename T>
+using indexed_set = tree<T,null_type,less<T>,rb_tree_tag, tree_order_statistics_node_update>;
 
 #define val find_by_order //[]
 #define idx order_of_key 
 
 
 //Uso:
-indexed_set s;
+indexed_set<ll> s;
 s.insert(5);
 s.erase(5);
 ll x = s.val(0); //x = s[0]
