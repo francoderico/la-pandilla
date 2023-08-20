3347f1d0957b7a34ecaa94c1b54407a7ff46ac87
SebastianMestre <mestresebastianunr@gmail.com>
2023-08-20 20:07:24 -0300

    Agrega cambios propuestos y borra las macros

--- estructuras/indexed_set.cpp
+++ estructuras/indexed_set.cpp
@@ -1,15 +1,11 @@
 #include <ext/pb_ds/assoc_container.hpp> 
 using namespace __gnu_pbds;
-template<typename T>
-using indexed_set = tree<T,null_type,less<T>,rb_tree_tag, tree_order_statistics_node_update>;
-
-#define val find_by_order //[]
-#define idx order_of_key 
-
+template<typename Key, typename Val=null_type>
+using indexed_set = tree<Key,Val,less<Key>,rb_tree_tag,tree_order_statistics_node_update>;
 
 //Uso:
 indexed_set<ll> s;
 s.insert(5);
 s.erase(5);
-ll x = s.val(0); //x = s[0]
-int i = s.idx(5) //s[i] == 5
+ll val = s.find_by_order(0); // acceso por indice
+int idx = s.order_of_key(5); // busca indice del valor
