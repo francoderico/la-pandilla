c3d4a8e77541d84a1049f2dd374f258510f48423
francoderico <francoderico2001@gmail.com>
2023-09-12 20:05:16 -0300

    Para hacer rollback en cualquier estructura que permita ir agregando cosas, como union find

---
+++ estructuras/rollback.cpp
@@ -0,0 +1,14 @@
+// Permite hacer CTRL+Z hasta el estado en que se toma el snapshot
+// Uso: int tiempo = snapshot(); ...; restore(tiempo);
+vector<pair<int&, int>> changes;
+int snapshot() { return sz(changes); }
+void restore(int snap) {
+	while (sz(changes) > snap) {
+		auto [l, r] = changes.back(); changes.pp();
+		l = r;
+	}
+}
+int assign(int& l, int r) { // Reemplazar los a=b de la struct por assign(a, b)
+	changes.pb({l, l});
+	return l = r;
+}
