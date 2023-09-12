80b1d227a77433b0735a262576925d28611b04a4
francoderico <francoderico2001@gmail.com>
2023-09-12 16:40:20 -0300

    Comprime líneas

--- algoritmos/math/phi_orden.cpp
+++ algoritmos/math/phi_orden.cpp
@@ -1,32 +1,15 @@
-// Cantidad de numeros coprimos y menores a n, dada su factorizacion
-ll phi(ll n, map<ll, int> &F)
-{
+ll phi(ll n, map<ll, int> &F){
     ll ret = n;
     for(const auto &[p, _] : F) ret -= ret / p;
     return ret;
 }
-
 // O(factorizar + lg^2 n)
 // Orden de a (mod m), es decir, el menor k tal que a^k = 1 (mod m)
-// Existe sii gcd(a, m) = 1
-// Es un divisor de phi(m)
-ll orden(ll a, ll m)
-{
-	// assert(gcd(a, m) == 1);
-	map<ll, int> F;
-	fact(m, F);
-	ll ph = phi(m, F);
-
-	F.clear();
-	fact(ph, F);
-
-	ll ret = ph;
-
-	for(auto [p, e] : F)
-	{
-		while(e > 0 and expmod(a, ret/p, m) == 1) ret /= p, e --;
-	}
-
+// Existe sii gcd(a, m) = 1. Es un divisor de phi(m)
+ll orden(ll a, ll m){ // assert(gcd(a, m) == 1);
+	map<ll, int> F; fact(m, F);
+	ll ph = phi(m, F); ll ret = ph;
+	F.clear(); fact(ph, F);
+	for(auto [p, e] : F) while(e > 0 and expmod(a, ret/p, m) == 1) ret /= p, e --;
 	return ret;
 }
-
