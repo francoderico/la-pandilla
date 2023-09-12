1cdc03834d7fd9adfdccd3c1d9ccaa427a341582
francoderico <francoderico2001@gmail.com>
2023-09-12 16:47:30 -0300

    Comprime cosas y mueve min_cap al mcMF

--- algoritmos/flujo/hopcroft.cpp
+++ algoritmos/flujo/hopcroft.cpp
@@ -1,27 +1,25 @@
-// Matching maximo en bipartito en O(sqrt(V) E)
-// Los vertices izquierdos estan numerados en [0, n), los derechos en [0, m)
-struct Hopcroft{
+struct Hopcroft{ // Matching maximo en bipartito en O(sqrt(n)*m)
 	int n, m;
-	vector<vector<int>> g;    // [0,n)->[0,m)
+	vector<vector<int>> g;    // [0,n) -> [0,m)
 	vector<int> mt, inv, ds;  // mt  : [0..n) -> [0..m) contiene el matching n-m
 	                          // inv : [0..m) -> [0..n) contiene el matching inverso	
 	void init(int n_, int m_) { n = n_, m = m_, g.resize(n_); }
 	void add_edge(int u, int v) { g[u].pb(v); }
 	bool bfs(){
 		ds.assign(n, -1); queue<int> q;
-		forn(i, n) if(mt[i]<0) ds[i] = 0, q.push(i);
+		forn(i, n) if(mt[i] < 0) ds[i] = 0, q.push(i);
 		bool r = false;
 		while(not q.empty()){
 			int x = q.front(); q.pop();
-			for(auto y:g[x]){
-				if(inv[y]>=0 and ds[inv[y]]<0) ds[inv[y]] = ds[x]+1, q.push(inv[y]);
-				else if(inv[y]<0) r = true;
+			for(auto y : g[x]){
+				if(inv[y] >= 0 and ds[inv[y]] < 0) ds[inv[y]] = ds[x]+1, q.push(inv[y]);
+				else if(inv[y] < 0) r = true;
 			}
 		}
 		return r;
 	}
 	bool dfs(int x){
-		for(auto y:g[x]) if(inv[y]<0 or (ds[inv[y]] == ds[x]+1 and dfs(inv[y]))){
+		for(auto y : g[x]) if(inv[y] < 0 or (ds[inv[y]] == ds[x]+1 and dfs(inv[y]))){
 			inv[y] = x; mt[x] = y;
 			return true;
 		}
