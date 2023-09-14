83a3805965e80172aa87636082aef2caecafc394
SebastianMestre <mestresebastianunr@gmail.com>
2023-09-14 13:55:21 -0300

    comprime, comentarios

--- estructuras/indexed_set.cpp
+++ estructuras/indexed_set.cpp
@@ -2,10 +2,6 @@
 using namespace __gnu_pbds;
 template<typename Key, typename Val=null_type>
 using indexed_set = tree<Key,Val,less<Key>,rb_tree_tag,tree_order_statistics_node_update>;
-
-//Uso:
-indexed_set<ll> s;
-s.insert(5);
-s.erase(5);
-ll val = s.find_by_order(0); // acceso por indice
-int idx = s.order_of_key(5); // busca indice del valor
+// indexed_set<char> s;
+// char val = s.find_by_order(0); // acceso por indice
+// int idx = s.order_of_key('a'); // busca indice del valor
