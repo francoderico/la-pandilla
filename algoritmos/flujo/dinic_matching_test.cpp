3662389c1a08f530b5ccf42ebe581d7d02de67f3
francoderico <francoderico2001@gmail.com>
2023-09-25 18:00:55 -0300

    Modifica tests de los algoritmos de flujo

--- algoritmos/flujo/dinic_matching_test.cpp
+++ algoritmos/flujo/dinic_matching_test.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 #define forr(i,a,b) for(int i=int(a);i<int(b);++i)
 #define forn(i,n) forr(i,0,n)
-#define dforr(i,a,b) for(int i=(b)-1;i>=(a);--i)
+#define dforr(i,a,b) for(int i=int(b)-1;i>=int(a);--i)
 #define dforn(i,n) dforr(i,0,n)
+#define fore(e,c) for(const auto &e : (c))
 #define db(v) cerr<<#v" = "<<(v)<<'\n'
-#define vecp(v) cerr<<#v<<" = "; for(auto ee:v)cerr<<ee<<' '; cerr<<'\n'
 #define nn cout<<'\n'
 #define sz(v) (int(v.size()))
-#define all(v) v.begin(), v.end()
+#define all(v) begin(v), end(v)
 #define pb push_back
 #define pp pop_back
 #define fst first
@@ -22,7 +22,6 @@ typedef unsigned long long ull;
 typedef long double ld;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
-typedef vector<ll> vll;
 
 const ll MAXN = 2e5+100;
 const ll INF = 1e18+100;
@@ -30,65 +29,56 @@ const ll MOD = 1e9+7;
 const ld EPS = 1e-9;
 const ld PI = acosl(-1);
 
-struct Dinic
-{
-	int nodes, src, dst;
-	vector<int> dist, q, work;
+struct Dinic{ // O(n^2 * m)
+	int n, src, dst;
+	vector<int> dist, q, work; // dist: distancias desde S, sin ponderar
 	struct edge {int to, rev; ll f, cap;};
-	vector<vector<edge>> g;
-	
-    Dinic(int x) : nodes(x), dist(x), q(x), work(x), g(x) {}
-	
-    void add_edge(int s, int t, ll cap)
-    {
-		g[s].pb((edge){t, sz(g[t]), 0, cap});
-		g[t].pb((edge){s, sz(g[s])-1, 0, 0});
+	vector<vector<edge>> g; vector<ll> dd;
+	Dinic(int n_) : n(n_), dist(n_+2), q(n_+2), work(n_+2), g(n_+2), dd(n_+2) {} // Deja espacio para el min_cap
+	void add_edge(int s, int t, ll cap, ll mincap = 0){
+		// assert(0 <= mincap and mincap <= cap);
+		dd[s] += mincap; dd[t] -= mincap;
+		g[s].pb((edge){t, sz(g[t]),   0, cap-mincap});
+		g[t].pb((edge){s, sz(g[s])-1, 0, 0}); // Residual: cap=0 y flujo negativo
 	}
-	
-    bool dinic_bfs()
-    {
+	bool dinic_bfs(){
 		fill(all(dist), -1); dist[src] = 0;
 		int qt = 0; q[qt++] = src;
-		for(int qh = 0; qh<qt; qh++)
-        {
+		for(int qh = 0; qh < qt; qh ++){
 			int u = q[qh];
-			forn(i, sz(g[u]))
-            {
-				edge &e = g[u][i]; int v = g[u][i].to;
-				if(dist[v]<0 and e.f<e.cap) dist[v] = dist[u]+1, q[qt++] = v;
+			fore(e, g[u]){
+				int v = e.to;
+				if(dist[v] < 0 and e.f < e.cap) dist[v] = dist[u]+1, q[qt++] = v;
 			}
 		}
 		return dist[dst] >= 0;
-	}
-	
-    ll dinic_dfs(int u, ll f)
-    {
-		if(u == dst)return f;
-		for(int &i = work[u]; i<sz(g[u]); i++)
-        {
+	}	
+	ll dinic_dfs(int u, ll f){
+		if(u == dst) return f;
+		for(int &i = work[u]; i < sz(g[u]); i ++){
 			edge &e = g[u][i];
-			if(e.cap <= e.f)continue;
+			if(e.cap <= e.f) continue;
 			int v = e.to;
-			if(dist[v] == dist[u]+1)
-            {
-				ll df = dinic_dfs(v, min(f, e.cap-e.f));
-				if(df > 0){e.f += df; g[v][e.rev].f -= df; return df;}
+			if(dist[v] == dist[u]+1){
+				ll df = dinic_dfs(v, min(f, e.cap - e.f));
+				if(df > 0){
+					e.f += df; g[v][e.rev].f -= df;
+					return df;
+				}
 			}
 		}
 		return 0;
 	}
-	
-    ll max_flow(int _src, int _dst)
-    {
+	ll max_flow(int _src, int _dst){
 		src = _src; dst = _dst;
 		ll result = 0;
-		while(dinic_bfs())
-        {
+		while(dinic_bfs()){
 			fill(all(work), 0);
 			while(ll delta = dinic_dfs(src, INF)) result += delta;
 		}
 		return result;
 	}
+	// ll max_flow_min_cap(int s, int t) esta en el mcMF
 };
 
 
---
+++ algoritmos/flujo/min_cost_max_flow_min_cap_test.cpp
@@ -0,0 +1,148 @@
+// https://codeforces.com/gym/101726/problem/I
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forr(i,a,b) for(int i=int(a);i<int(b);++i)
+#define forn(i,n) forr(i,0,n)
+#define dforr(i,a,b) for(int i=int(b)-1;i>=int(a);--i)
+#define dforn(i,n) dforr(i,0,n)
+#define fore(e,c) for(const auto &e : (c))
+#define db(v) cerr<<#v" = "<<(v)<<'\n'
+#define nn cout<<'\n'
+#define sz(v) (int(v.size()))
+#define all(v) begin(v), end(v)
+#define pb push_back
+#define pp pop_back
+#define fst first
+#define snd second
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+//const ll MAXN = 2e5+100;
+//const ll INF = 1e18+100;
+const ll MOD = 1e9+7;
+const ld EPS = 1e-9;
+const ld PI = acosl(-1);
+
+typedef ll tf;
+typedef ll tc;
+const tf INFFLOW = 1e9;
+const tc INFCOST = 1e9;
+struct MCF { // O(n^2 * m^2)
+	int n;
+	vector<tc> prio, pot; vector<tf> curflow; vector<int> prevedge, prevnode;
+	priority_queue<pair<tc, int>, vector<pair<tc, int>>, greater<pair<tc, int>>> q;
+	struct edge{int to, rev; tf f, cap; tc cost;};
+	vector<vector<edge>> g;
+	vector<tf> dd; tc dc = 0;
+	MCF(int n_) : n(n_), prio(n_+2), pot(n_+2), curflow(n_+2), prevedge(n_+2), prevnode(n_+2), g(n_+2), dd(n_+2) {}
+	void add_edge(int s, int t, tc cost, tf cap, tf mincap = 0){
+		// assert(0 <= mincap and mincap <= cap);
+		dd[s] += mincap; dd[t] -= mincap; dc += mincap*cost;
+		g[s].pb((edge){t, sz(g[t]), 0, cap-mincap, cost});
+		g[t].pb((edge){s, sz(g[s])-1, 0, 0, -cost});
+	}
+	pair<tf, tc> get_flow(int s, int t){
+		tf flow = 0; tc cost = 0;
+		while(1){
+			q.push({0, s});
+			fill(all(prio), INFCOST); 
+			prio[s] = 0; curflow[s] = INFFLOW;
+			while(not q.empty()){
+				auto [d, u] = q.top();
+				q.pop();
+				if(d != prio[u]) continue;
+				forn(i, sz(g[u])){
+					edge &e = g[u][i];
+					int v = e.to;
+					if(e.cap <= e.f) continue;
+					tc nprio = prio[u] + e.cost + pot[u] - pot[v];
+					if(prio[v] > nprio){
+						prio[v] = nprio;
+						q.push({nprio, v});
+						prevnode[v] = u; prevedge[v] = i;
+						curflow[v] = min(curflow[u], e.cap-e.f);
+					}
+				}
+			}
+			if(prio[t] == INFCOST) break;
+			forn(i, n) pot[i] += prio[i];
+			tf df = min(curflow[t], INFFLOW-flow);
+			flow += df;
+			for(int v = t; v != s; v = prevnode[v]){
+				edge &e = g[prevnode[v]][prevedge[v]];
+				e.f += df; g[v][e.rev].f -= df;
+				cost += df*e.cost;
+			}
+		}
+		return {flow, cost};
+	}
+	pair<tf, tc> get_flow_min_cap(int s, int t){	// villear?
+		add_edge(t, s, 0, INFFLOW);
+		ll w = 0;
+		forn(i, n){
+			if     (dd[i] > 0) add_edge(i, n+1, 0,  dd[i]), w += dd[i];
+			else if(dd[i] < 0) add_edge(n, i,   0, -dd[i]);
+		}
+		auto [f, c] = get_flow(n, n+1);
+		if(f != w) return {-1, -1};
+		forn(i, n) if(dd[i] != 0) g[i].pp();
+		auto [ff, cc] = get_flow(s, t); g[s].pp(), g[t].pp();
+		return {ff, c + cc + dc};
+	}
+};
+
+void dfs(int v, vector<vector<int>> &g, vector<bool> &vis){
+	vis[v] = true;
+	for(auto u : g[v]) if(not vis[u]) dfs(u, g, vis);
+}
+
+int n;
+vector<vector<int>> g, gg;
+vector<bool> vis;
+
+bool is_scc(){
+	vis.assign(n, false);
+	dfs(0, g, vis);
+	forn(u, n) if(not vis[u]) return false;
+	forn(u, n) for(auto v : g[u]) gg[v].pb(u);
+	vis.assign(n, false);
+	dfs(0, gg, vis);
+	forn(u, n) if(not vis[u]) return false;
+	return true;
+}
+
+void solve(){
+	int m; cin >> n >> m;
+	MCF net(n+2);
+	g.assign(n, {}); gg.assign(n, {});
+	forn(_, m){
+		int u, v, d; cin >> u >> v >> d; u --, v --;
+		net.add_edge(u, v, d, INFFLOW, 1);
+		g[u].pb(v);
+	}
+	
+	if(not is_scc()){
+		cout << -1, nn;
+		return;
+	}
+
+	auto [f, c] = net.get_flow_min_cap(n, n+1);
+	cout << c, nn;
+}
+
+int main(){
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t; while(t --)
+	solve();
+}
