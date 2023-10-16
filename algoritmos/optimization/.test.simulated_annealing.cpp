ba4feaeef3f1a479c5e804b5cd9d8dd605b9aca3
Federico Bersano <bersanofede@gmail.com>
2023-10-15 22:59:15 -0300

    Mejorar Simulated Annealing (#67)
    El código es menos de la mitad de lo que era antes.

--- algoritmos/optimization/.test.simulated_annealing.cpp
+++ algoritmos/optimization/.test.simulated_annealing.cpp
@@ -14,33 +14,34 @@ using namespace std;
 void solve() {
 	int n;
 	cin>>n;
-	vector<int>order;
+	vector<int> order;
 	forn(i, n) {
 		const int size = i + 1;
 		int num;
 		cin>>num;
-		while (num--) {order.push_back(size);}
+		while (num--) order.push_back(size);
 	}
 	const int groups = sz(order);
 	int l, r;
-	auto stir = [&] {
-		l = rng.random_int(groups);
-		r = rng.random_int(groups);
+	auto see = [&] {
+		l = rng.integer(groups);
+		r = rng.integer(groups);
 		int len = 1, last = len;
 		swap(order[l], order[r]);
 		fore(curr, order){
-			while (len < curr) {len = ++last;}
+			while (len < curr) len = ++last;
 			len -= curr;
-			if (len) {--len;}
+			if (len) --len;
 		}
 		swap(order[l], order[r]);
 		return last;
 	};
-	auto save = [&] {swap(order[l], order[r]);};
-	SimulatedAnnealing<int> sa(stir, save);
-	const int rows = sa.simulate(0.95).peek_lowest_energy();
-	if (rows <= 12) {cout<<rows<<'\n';}
-	else {cout<<"impossible\n";}
+	auto upd = [&] {swap(order[l], order[r]);};
+	Annealing sa{see, upd};
+	sa.simulate(0.95);
+	const int rows = sa.low;
+	if (rows <= 12) cout<<rows<<'\n';
+	else cout<<"impossible\n";
 }
 
 int main() {
--- algoritmos/optimization/simulated_annealing.hpp
+++ algoritmos/optimization/simulated_annealing.hpp
@@ -1,110 +1,40 @@
 using my_clock = chrono::steady_clock;
 
-/// Useful tool for generating random numbers easily.
-class RandomNumberGenerator {
-	mt19937 engine;
-public:
-	RandomNumberGenerator():
-		engine(my_clock::now().time_since_epoch().count()) {}
-	/// Return random integer in [0,n).
-	template<typename Int=int>
-	Int random_int(Int n) {return random_int<Int>(0, n);}
-	/// Return random integer in [a,b).
-	template<typename Int=int>
-	Int random_int(Int a, Int b) {
-		return uniform_int_distribution<Int>(a, b-1)(engine);
-	}
-	/// Return random real in [0,r).
-	template<typename Real=double>
-	Real random_real(Real r=1.) {return random_real<Real>(0., r);}
-	/// Return random real in [a,b).
-	template<typename Real=double>
-	Real random_real(Real a, Real b) {
-		return uniform_real_distribution<Real>(a, b)(engine);
-	}
-	/// Shuffle range [first,last) randomly.
-	template<typename RandomIt>
-	void shuffle(RandomIt first, RandomIt last) {
-		std::shuffle(first, last, engine);
-	}
+struct Random {
+	mt19937_64 engine;
+	Random(): engine(my_clock::now().time_since_epoch().count()) {}
+	template<class Int>Int integer(Int n) {return integer<Int>(0, n);} // `[0,n)`
+	template<class Int>Int integer(Int l, Int r)
+		{return uniform_int_distribution{l, r-1}(engine);} // `[l,r)`
+	double real() {return uniform_real_distribution{}(engine);} // `[0,1)`
 } rng;
 
-/// Useful tool for fetching the time since the start of program execution.
-class TimeKeeper {
-	using time_point = my_clock::time_point;
-	time_point start = my_clock::now();
-public:
-	/// Reset the timer.
-	void reset() {start = my_clock::now();}
-	/// Return the current time in the specified period, seconds by default.
-	template<typename Rep=double, class Period=ratio<1>>
-	Rep elapsed() {
-		const time_point now = my_clock::now();
-		return chrono::duration<Rep, Period>(now - start).count();
+struct Timer {
+	using time = my_clock::time_point;
+	time start = my_clock::now();
+	double elapsed() { // Segundos desde el inicio.
+		time now = my_clock::now();
+		return chrono::duration<double>(now - start).count();
 	}
 } timer;
 
-/// Encapsulation of the simulated annealing algorithm.
-///
-/// It just takes two argument functions, one for temporarily changing the
-/// state of the system slightly and one for saving that internal state.
-template<typename Energy>
-class SimulatedAnnealing {
-	using stir_function = function<const Energy()>;
-	using save_function = function<void()>;
-	const stir_function stir;
-	const save_function save;
-	Energy curr_energy, coldest;
-	/// Initialize the state of the system and return the corresponding energy.
-	const Energy initialize_state() const {
-		const Energy energy = stir();
-		return (save(), energy);
-	}
-	/// Return the current temperature of the system.
-	///
-	/// Usually one will want to modify this definition, depending on the problem
-	/// being solved, so that the temperature is not as small in the beginning, so
-	/// long as it's decreasing and tending to zero.
-	double measure_temp(const double percentage_left) const {
-		return percentage_left;
-	}
-	/// Return the probability of accepting the new state corresponding to the
-	/// given energy.
-	double accept_prob(const Energy new_energy, const double temp) const {
-		const double delta = static_cast<double>(curr_energy - new_energy);
-		if (delta >= 0.) {return 1.;}
-		return exp(delta/temp);
-	}
-public:
-	SimulatedAnnealing(const stir_function _stir, const save_function _save):
-		stir(move(_stir)),
-		save(move(_save)),
-		curr_energy(initialize_state()),
-		coldest(curr_energy) {}
-	/// Execute the simulation until the time limit is reached.
-	SimulatedAnnealing& simulate(const double time_limit=1.) {
-		const double initial_time = timer.elapsed();
-		for (
-			double elapsed_time = timer.elapsed() - initial_time;
-			elapsed_time < time_limit;
-			elapsed_time = timer.elapsed() - initial_time
-		) {
-			const Energy new_energy = stir();
-			coldest = min(coldest, new_energy);
-			const double temp = measure_temp(1-elapsed_time/time_limit);
-			if (accept_prob(new_energy, temp) >= rng.random_real()) {
-				curr_energy = (save(), new_energy);
+template<class See,class Upd>struct Annealing {
+	using energy = invoke_result_t<See>;
+	energy curr, low;
+	See see;
+	Upd upd;
+	Annealing(See _see, Upd _upd): see{_see}, upd{_upd}
+		{curr = low = see(), upd();}
+	void simulate(double s, double mult=1) { // Simula por `s` segundos.
+		double t0 = timer.elapsed();
+		for (double t = t0; t-t0 < s; t = timer.elapsed()) {
+			energy near = see();
+			auto delta = double(curr - near);
+			if (delta >= 0) upd(), curr = near, low = min(low, curr);
+			else {
+				double temp = mult * (1 - (t-t0)/s);
+				if (exp(delta/temp) > rng.real()) upd(), curr = near;
 			}
 		}
-		return *this;
 	}
-	/// Return the last energy saved by the system.
-	///
-	/// This may or may not be the lowest energy of the simulation, although in
-	/// most problems it is. Useful for when one needs to now the energy
-	/// corresponding to the last state of the system, since storing the best
-	/// state can be inefficient.
-	const Energy peek_last_energy() const {return curr_energy;}
-	/// Return the lowest energy reached by the simulation at any point in time.
-	const Energy peek_lowest_energy() const {return coldest;}
 };
