6640377ffc31bc4b955913bd8f1eda40cd316fd1
Federico Bersano <bersanofede@gmail.com>
2023-10-17 17:12:25 -0300

    Cambiar un par de líneas

--- strings/hashing.cpp
+++ strings/hashing.cpp
@@ -8,7 +8,7 @@ struct StrHash { // Hash polinomial con exponentes decrecientes.
 			hs[k].resize(n+1), bs[k].resize(n+1, 1);
 			forn(i, n) {
 				hs[k][i+1] = (hs[k][i] * b + s[i]) % ms[k];
-				bs[k][i+1] = bs[k][i] * b % ms[k];
+				bs[k][i+1] =  bs[k][i] * b         % ms[k];
 			}
 		}
 	}
@@ -16,7 +16,7 @@ struct StrHash { // Hash polinomial con exponentes decrecientes.
 		ll h[2];
 		forn(k, 2) {
 			h[k] = hs[k][idx+len] - hs[k][idx] * bs[k][len] % ms[k];
-			h[k] += h[k] < 0 ? ms[k] : 0;
+			if (h[k] < 0) h[k] += ms[k];
 		}
 		return (h[0] << 32) | h[1];
 	}
