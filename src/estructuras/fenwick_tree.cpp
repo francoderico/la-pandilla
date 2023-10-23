0297564e073fb88633748d7ebbfd8082bff6e5ad
SebastianMestre <mestresebastianunr@gmail.com>
2023-10-23 20:01:29 -0300

    Fenwick tree villeado y fenwick con range-update/range-query

--- src/estructuras/fenwick_tree.cpp
+++ src/estructuras/fenwick_tree.cpp
@@ -1,20 +1,12 @@
-struct Fenwick  //0-indexed
-{
-    int n;
-    vector<int> tree;
-    Fenwick(int n_) : n(n_), tree(n_+1, 0) {}
+struct Fenwick { // Requiere `int const MAXN;`
+int ft[MAXN+1];  // Uso: ft.u(idx, val); cout << ft.q(idx);
+int u(int i0, int x) { for (int i=i0+1; i<=MAXN; i+=i&-i) ft[i]+=x; }
+ll q(int i0) { ll x=0; for (int i=i0; i>0; i-=i&-i) x+=ft[i]; return x; }
+ll q(int l, int r) { return q(r) - q(l); } };
 
-    void upd(int i, int x)  //Le sumo x al elemento en posicion i
-    {
-        for(i += 1; i <= n; i += i & -i) tree[i] += x;
-    }
-
-    int query(int b)        //Devuelve la suma de los elementos en [0..b)
-    {
-        int s = 0;
-        
-        for(int i = b; i; i -= i & -i) s += tree[i];
-        
-        return s;
-    }
-};
+struct RangeFT {   // Requiere `struct Fenwick`
+Fenwick rate, err; // Uso: ft.u(l, r, val); cout << ft.q(l, r);
+void u(int l, int r, int x) { // range update
+	rate.u(l, x); rate.u(r, -x); err.u(l, -x*l); err.u(r, x*r); }
+ll q(int i) { return rate.q(i) * i + err.q(i); } // prefix query
+ll q(int l, int r) { return q(r) - q(l); } }; // range query
