9bd4d7e4e97979e38b82705614f0ff252cebc10f
SebastianMestre <mestresebastianunr@gmail.com>
2023-10-25 11:22:05 -0300

    Implementa cambios sugeridos

--- src/estructuras/fenwick_tree.cpp
+++ src/estructuras/fenwick_tree.cpp
@@ -1,12 +1,10 @@
-struct Fenwick { // Requiere `int const MAXN;`
-int ft[MAXN+1];  // Uso: ft.u(idx, val); cout << ft.q(idx);
-int u(int i0, int x) { for (int i=i0+1; i<=MAXN; i+=i&-i) ft[i]+=x; }
-ll q(int i0) { ll x=0; for (int i=i0; i>0; i-=i&-i) x+=ft[i]; return x; }
-ll q(int l, int r) { return q(r) - q(l); } };
+struct Fenwick { // 0-indexed, query [0, i), update [i]
+	int ft[MAXN+1]; // Uso: ft.u(idx, val); cout << ft.q(idx);
+	int u(int i0, int x) { for (int i=i0+1; i<=MAXN; i+=i&-i) ft[i]+=x; }
+	ll q(int i0){ ll x=0; for (int i=i0; i>0; i-=i&-i) x+=ft[i]; return x; } };
 
-struct RangeFT {   // Requiere `struct Fenwick`
-Fenwick rate, err; // Uso: ft.u(l, r, val); cout << ft.q(l, r);
-void u(int l, int r, int x) { // range update
-	rate.u(l, x); rate.u(r, -x); err.u(l, -x*l); err.u(r, x*r); }
-ll q(int i) { return rate.q(i) * i + err.q(i); } // prefix query
-ll q(int l, int r) { return q(r) - q(l); } }; // range query
+struct RangeFT { // 0-indexed, query [0, 1), update [l, r)
+	Fenwick rate, err; // Uso: ft.u(l, r, val); cout << ft.q(l, r);
+	void u(int l, int r, int x) { // range update
+		rate.u(l, x); rate.u(r, -x); err.u(l, -x*l); err.u(r, x*r); }
+	ll q(int i) { return rate.q(i) * i + err.q(i); } }; // prefix query
