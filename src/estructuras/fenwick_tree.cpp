e2a4538963e9f46d970353d48aaa41aabcbe208d
francoderico <francoderico2001@gmail.com>
2024-04-02 12:32:52 -0300

    Un caracter menos, para que entre en una línea

--- src/estructuras/fenwick_tree.cpp
+++ src/estructuras/fenwick_tree.cpp
@@ -1,7 +1,7 @@
 struct Fenwick { // 0-indexed, query [0, i), update [i]
 	int ft[MAXN+1]; // Uso: ft.u(idx, val); cout << ft.q(idx);
 	int u(int i0, int x) { for (int i=i0+1; i<=MAXN; i+=i&-i) ft[i]+=x; }
-	ll q(int i0){ ll x=0; for (int i=i0; i>0; i-=i&-i) x+=ft[i]; return x; } };
+	ll q(int i0){ ll x=0; for (int i=i0; i>0; i-=i&-i) x+=ft[i]; return x; }};
 
 struct RangeFT { // 0-indexed, query [0, 1), update [l, r)
 	Fenwick rate, err; // Uso: ft.u(l, r, val); cout << ft.q(l, r);
