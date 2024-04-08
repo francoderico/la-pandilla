12509e7b423420801401646ea8cfdf14a9249ead
francoderico <francoderico2001@gmail.com>
2024-04-08 15:54:37 -0300

    Agrega union find

---
+++ src/grafos/.test.union_find.cpp
@@ -0,0 +1,44 @@
+// SPOJ MST - AC
+// http://www.spoj.com/problems/MST/
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,to=b;i<to;++i)
+using namespace std;
+typedef long long ll;
+
+#define MAXN 10005
+
+vector<int> uf(MAXN, -1);
+int uf_find(int x) { return uf[x]<0 ? x : uf[x] = uf_find(uf[x]); }
+bool uf_join(int x, int y){ // True sii x e y estan en != componentes
+	x = uf_find(x); y = uf_find(y);
+	if(x == y) return false;
+	if(uf[x] > uf[y]) swap(x, y);
+	uf[x] += uf[y]; uf[y] = x; return true;
+}
+vector<pair<ll,pair<int,int> > > es; // edges (cost,(u,v))
+ll kruskal(){  // assumes graph is connected
+	sort(es.begin(),es.end());
+	ll r=0;
+	fore(i,0,es.size()){
+		int x=es[i].snd.fst,y=es[i].snd.snd;
+		if(uf_join(x,y))r+=es[i].fst; // (x,y,c) belongs to mst
+	}
+	return r; // total cost
+}
+
+int n,m;
+
+int main(){
+	scanf("%d%d",&n,&m);
+	while(m--){
+		int x,y;ll c;
+		scanf("%d%d%lld",&x,&y,&c);x--;y--;
+		es.pb({c,{x,y}});
+	}
+	printf("%lld\n",kruskal());
+	return 0;
+}
