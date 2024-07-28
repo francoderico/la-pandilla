8e397c09001e13aef99e33d345a05fcff257375e
francoderico <francoderico2001@gmail.com>
2024-07-27 21:11:09 -0300

    Cambia cuentita de la sparse table

--- src/estructuras/sparse_table.cpp
+++ src/estructuras/sparse_table.cpp
@@ -3,7 +3,7 @@
 struct SparseTable {
 	vector<vector<int>> st;
 	void init(int* a, int n) {
-		int K = 31-__builtin_clz(2*n-1);
+		int K = 31-__builtin_clz(2*n);
 		st.assign(K, vector<int>(n));
 		forn(i, n) st[0][i] = a[i];
 		forr(k,1,K) forn(i,n-(1<<k)+1)
