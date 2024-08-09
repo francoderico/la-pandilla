766f3a719843546474ed44dd5d19a3b465e55154
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2024-08-09 11:04:19 -0300

    reemplaza ternary real con golden search (#98)

--- src/optimization/.test.ternary_search.real.cpp
+++ src/optimization/.test.ternary_search.real.cpp
@@ -10,7 +10,16 @@ using namespace std;
 
 using ll = long long;
 
-#include "ternary_search.hpp"
+// para error < EPS, usar iters=log((r-l)/EPS)/log(1.618)
+template<class Fun>double golden(Fun f, double l, double r, int iters){
+	double const ratio = (3-sqrt(5))/2;
+	double x1=l+(r-l)*ratio, x2=r-(r-l)*ratio, f1=f(x1), f2=f(x2);
+	while (iters--) {
+		if (f1 > f2) l=x1, x1=x2, f1=f2, x2=r-(r-l)*ratio, f2=f(x2);
+		else         r=x2, x2=x1, f2=f1, x1=l+(r-l)*ratio, f1=f(x1);
+	}
+	return (l+r)/2;
+}
 
 int n;
 vector<int> a;
@@ -35,7 +44,7 @@ void solve() {
 		return max(sum(m, 1), sum(m, -1));
 	};
 	double l = -2e9, r = 2e9;
-	double x = ternary(f, l, r, 128);
+	double x = golden(f, l, r, 128);
 	cout<<f(x)<<'\n';
 }
 
